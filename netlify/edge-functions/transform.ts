414046b4c3d30cbc1d1529b613c4bedd70187920
Jordan Whistler <whistler.5@osu.edu>
2023-01-27 17:20:01 -0500

    switch

--- netlify/edge-functions/transform.ts
+++ netlify/edge-functions/transform.ts
@@ -4,15 +4,22 @@ export default async (request: Request, context: Context) => {
   const url = new URL(request.url);
 
   // Look for the query parameter, and return if we don't find it
-  if (url.searchParams.get("page") !== "one") {
-    return context.rewrite("/posts/one-more-post");
+  switch (true) {
+    case (url.searchParams.get("page") === "one"):
+      return context.rewrite("/posts/one-more-post");
+    case (url.searchParams.get("page") === "two"):
+      return context.rewrite("/posts/example-post");
+    case (url.searchParams.get("page") === "three"):
+      return context.rewrite("/posts/test-post");
+    case (url.searchParams.get("upper") === "true"):
+      console.log(`Transforming the response from this ${url}`);
+
+      const response = await context.next();
+
+      const text = await response.text();
+      return new Response(text.toUpperCase(), response);
   }
-  console.log(`Transforming the response from this ${url}`);
-
-  const response = await context.next();
-
-  const text = await response.text();
-  return new Response(text.toUpperCase(), response);
+  return
 };
 
 export const config = { path: '/' };
